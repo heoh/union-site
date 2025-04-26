68c8bb2773878811cee30f9f4dda2014f9d1fe30
heoh <h0h6h2h5@gmail.com>
2025-04-26 15:48:39 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from 'react-router';
+import Home from './pages/Home';
+import About from './pages/About';
+
+function App() {
+  return (
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+    </Routes>
+  );
+}
+
+export default App;
